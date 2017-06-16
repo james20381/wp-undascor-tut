ae99fd2b8c2cbf18b69c304618e8d9e0807e1804
James20381 <pikej307@gmail.com>
2017-06-16 23:06:52 +1000

    page.php - added zzzzzzz
    Changed template to templatezzzzzzz in page.php wp-undascor-tut theme

--- wp-content/themes/wp-undascor-tut/page.php
+++ wp-content/themes/wp-undascor-tut/page.php
@@ -5,7 +5,7 @@
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
- * different template.
+ * different templatezzzzzz.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
